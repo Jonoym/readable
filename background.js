1150990c04c8c8f7dd239d2bed29fe29d19e7613
Jonoym <jonoym@github.com>
2022-06-16 19:39:42 +1000

    Changed parsing algorithm.

--- background.js
+++ background.js
@@ -7,30 +7,53 @@ chrome.action.onClicked.addListener((tab) => {
 
 const boldText = () => {
 
-  let parsedText = document.body.outerHTML.split(/\<[^\>]*\>/);
-  let parsedElements = document.body.outerHTML.split(/\>[^<]*\</);
+    if (document.getElementById("ReadableCheck") !== null) {
+        chrome.storage.local.get( "outerHTML", ({ outerHTML }) => {
+            document.body.outerHTML = outerHTML;
+        })
+    } else {
+        let outerHTML = document.body.outerHTML;
 
-  let newHTML = "";
+        chrome.storage.local.set({ outerHTML });
 
-  for (let j = 0; j < parsedText.length; j++) {
-      let newWords = "";
-      let words = parsedText[j].split(/ |-/);
-      for (let k = 0; k < words.length; k++) {
-          let word = words[k];
-          if (word.length == 1) {
-              newWords += `<b>${word}</b> `;
-          } else {
-              newWords += `<b>${word.substring(0, word.length / 2)}</b>${word.substring(word.length / 2)} `;
-          }
-      }
-      newHTML += newWords;
-      if (j === 0) {
-          newHTML += `${parsedElements[j]}>`;
-      } else if (j === parsedText.length - 2) {
-          newHTML += `<${parsedElements[j]}`;
-      } else if (j !== parsedText.length - 1) {
-          newHTML += `<${parsedElements[j]}>`;
-      }
-  }
-  document.body.outerHTML = newHTML;
+        let parsedText = document.body.outerHTML.split(/\<[^\>]*\>/);
+        let parsedElements = document.body.outerHTML.split(/\>[^<]*\</);
+    
+        let newHTML = "";
+        let isParagraph = false;
+        for (let j = 0; j < parsedText.length; j++) {
+            let newWords = "";
+            let words = parsedText[j].split(/ |-/);
+            for (let k = 0; k < words.length; k++) {
+                let word = words[k];
+                if (isParagraph) {
+                    newWords += `<b>${word.substring(0, word.length / 2 + 1)}</b>${word.substring(word.length / 2 + 1)} `;
+                } else {
+                    newWords += word + " ";
+                }
+            }
+            newHTML += newWords;
+            if (j === 0) {
+                newHTML += `${parsedElements[j]}>`;
+            } else if (j === parsedText.length - 2) {
+                newHTML += `<${parsedElements[j]}`;
+            } else if (j !== parsedText.length - 1) {
+                newHTML += `<${parsedElements[j]}>`;
+            }
+            if (j !== parsedText.length - 1) {
+                if (parsedElements[j].substring(0, 2) === "p ") {
+                    isParagraph = true;
+                }
+                if (parsedElements[j].substring(0, 2) === "/p") {
+                    isParagraph = false;
+                }
+            }
+        }
+    
+        document.body.outerHTML = newHTML;
+    
+        var readableCheck = document.createElement('div');
+        readableCheck.id = "ReadableCheck";
+        document.body.appendChild(readableCheck);
+    }
 }
